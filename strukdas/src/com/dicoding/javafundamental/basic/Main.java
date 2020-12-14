08145f646b9857d84e2f5e495799e5146fec09a3
Alberto0150 <albertosanjaya2@gmail.com>
2020-12-14 23:22:15 +0700

    multi package call
    1 class in 1 package
    3 class in 1 package

--- strukdas/src/com/dicoding/javafundamental/basic/Main.java
+++ strukdas/src/com/dicoding/javafundamental/basic/Main.java
@@ -1,8 +1,17 @@
 package com.dicoding.javafundamental.basic;
 
+import com.dicoding.javafundamental.basic.kendaraan.*;
+import com.dicoding.javafundamental.basic.musik.Gitar;
+
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+
+        Gitar.bunyi();
+
+        Mobil.jumlahBan();
+        Motor.jumlahBan();
+        Kereta.jumlahBan();
     }
 }
